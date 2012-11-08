3c856a298f2ac418df5e023e6b53b8d7f8fafabe
Remco Wendt <remco@maykinmedia.nl>
2012-11-08 02:54:47 +0100

    Check for actual talk content in response

--- src/sytycnluug/ratezzz/tests.py
+++ src/sytycnluug/ratezzz/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 
+from .models import Talk
+
 
 class TalkViewTests(TestCase):
+    def setUp(self):
+        self.talk = Talk.objects.create(name='My preciousssss...', speakers='Gollum')
+
     def test_index_page_returns_talk(self):
         # The index page of the ratezzz app should exist
         response = self.client.get('/ratezzz/')
-        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'My preciousssss...')
