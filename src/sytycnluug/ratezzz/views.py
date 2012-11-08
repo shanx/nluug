70b7f463eb6f267ed60c464a58081a7f71cb85e5
Remco Wendt <remco@maykinmedia.nl>
2012-11-08 03:02:58 +0100

    Added talk content to view using a template

--- src/sytycnluug/ratezzz/views.py
+++ src/sytycnluug/ratezzz/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.views.generic import  View
+
+from .models import Talk
 
 
 class TalkView(View):
     def get(self, request):
-        return HttpResponse()
+        return render_to_response('talk_list.html', {'talks': Talk.objects.all()})
