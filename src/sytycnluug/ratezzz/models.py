a9bab39cfaf65312b79515158b478517a32e5a3b
Remco Wendt <remco@maykinmedia.nl>
2012-11-08 01:58:47 +0100

    Added talk model

--- src/sytycnluug/ratezzz/models.py
+++ src/sytycnluug/ratezzz/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Talk(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    speakers = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return u'{0} by {1}'.format(self.name, self.speakers)
+
+    def get_absolute_url(self):
+        return '/ratezzz/{0}/'.format(self.pk)
+
