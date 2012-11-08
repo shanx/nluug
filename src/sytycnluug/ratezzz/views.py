04f3d21f1f01d93577b0d92073affb4c06758233
Remco Wendt <remco@maykinmedia.nl>
2012-11-08 03:40:41 +0100

    Added post view for talks (to post ratings), fixes the test again. Yay!

--- src/sytycnluug/ratezzz/views.py
+++ src/sytycnluug/ratezzz/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.generic import  View
 
@@ -7,3 +8,9 @@ from .models import Talk
 class TalkView(View):
     def get(self, request):
         return render_to_response('talk_list.html', {'talks': Talk.objects.all()})
+
+    def post(self, request, pk):
+        talk = Talk.objects.get(pk=pk)
+        talk.rating_set.create(rating = request.POST['rating'])
+
+        return HttpResponse()
--- src/sytycnluug/urls.py
+++ src/sytycnluug/urls.py
@@ -5,4 +5,5 @@ from sytycnluug.ratezzz.views import TalkView
 
 urlpatterns = patterns('',
     url(r'^ratezzz/$', TalkView.as_view()),
+    url(r'^ratezzz/(?P<pk>\d+)/$', TalkView.as_view()),
 )
